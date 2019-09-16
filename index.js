8afad90636db8bb951e1e6b375e106274a200039
MohssineElAttari <14mohssine@gmail.com>
2019-09-16 13:05:42 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * @format
+ */
+import React from 'react';
+import { AppRegistry, Text } from 'react-native';
+// import App from './App';
+import {name as appName} from './app.json';
+const App = () => {
+    return (<Text>HB-WEB // mohssine</Text>);
+}
+AppRegistry.registerComponent(appName, () => App);
